992f868a3335227890870bd150a57c0c67309eef
Phonso <abdoulazizc867@gmail.com>
2024-10-24 00:50:58 +0000

    Magic number after check the correction try except!

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 
+def ask_number(min_num, max_num):
+    number_int = 0
+    while number_int == 0:
+        number_str = input(f"Quel est le nombre magique (entre {min_num} et {max_num})? : ")
+        try:
+            number_int = int(number_str)
+        except ValueError:
+            print("ERREUR: Tu dois entrer un nombre ! Retente ta chance.")
+    return number_int
+
+
 MIN_NUMBER = 1
 MAX_NUMBER = 10
 MAGIC_NUMBER = 5
 
-def ask_number(min_num, max_num):
-    user_number = int(input(f"Quel est le nombre magique (entre {min_num} et {max_num})? : "))
-    return user_number
-
-
 number = 0
-def verify_number():
-    if number < 5:
+while not number == MAGIC_NUMBER:
+    number = ask_number(MIN_NUMBER, MAX_NUMBER)
+    if number < MAGIC_NUMBER:
         print("Le nombre est plus grand !")
-    elif number > 5:
+    elif number > MAGIC_NUMBER:
         print("Le nombre est plus petit !")
     else:
         print("Bravo, tu as trouvé le nombre magique !")
-
-
-while number != 5:
-    number = ask_number(MIN_NUMBER, MAX_NUMBER)
-    verify_number()
